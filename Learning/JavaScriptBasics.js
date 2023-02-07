b599f6534b6de812cf81ee7cc4aa10c85c6ddae9
“Aditya-Iyer7” <adityaiyer.m@gmail.com>
2023-02-06 19:48:08 -0800

    starting JS basics

---
+++ Learning/JavaScriptBasics.js
@@ -0,0 +1,46 @@
+//this document contains the following 
+/* 
+1. Commenting 
+2. Data Types
+3. Declaring Variables  
+ */
+
+//in line comment 
+/*block 
+comments (note that comments are exactly like java) */
+
+//declare variables 
+/* JS data types:
+1. undefined (something that hasn't been defined, for eg a variable that has been created but not set to anything)
+2. null (variable created and set to nothing)
+3. boolean
+4. string 
+5. symbol
+6. number
+7. object 
+
+ */
+
+var myName = "Batman"; //var is short for variable
+myName = 8; //variable can take on multiple data types like python, not fixed like java
+
+let ourName = "Avengers";
+const pi = 3.14; 
+
+//var, can be used throughout the whole program
+//let can be used only in the scope of where we're declaring it 
+//const is a variable that can never change, essentially makes a variable immutable 
+
+var a; //declaring
+var b = 2; //assigning 
+
+console.log(b); //allows you to see things in the console, expected output in this is 2 
+
+//variables and functions ARE case sensitive 
+
+var sum = 10 + 20 ;
+var diff = 45 - 15 ;
+
+var mul = 10 * 10 ;
+
+var div = 66 / 6 ; 
